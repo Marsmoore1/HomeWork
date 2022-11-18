28ea15de142cc73696d2f469ab0522635bdf1f77
Кирилл Шевель <shevelkirill2011@gmail.com>
2022-11-18 14:05:43 +0300

    ex9_2 added

---
+++ ex9_2/src/Reader.java
@@ -0,0 +1,78 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+public class Reader {
+    Scanner sc=new Scanner(System.in);
+    String name, surname, dadName, faculty, birthDate, phone;
+    int tickNumber;
+    LinkedList<Book> bookList=new LinkedList<Book>(); //создаем коллекцию из экземпляров класса Book
+
+    Reader() {
+    }
+    public void takeBook(Book book){
+        if(bookList.size()<10) {
+            bookList.add(book);
+            System.out.println(getFIO()+"взял книгу: "+book.getName());
+            return;
+        }
+        else if (bookList.size()==10){
+            System.out.println("Читатель уже имеет на руках 10 книг");
+        }
+    }
+    public String getFIO(){
+        return name +" "+ surname.charAt(0)+"."+ dadName.charAt(0)+". ";
+    }
+    public Book returnBook(String bookName){
+        Book ret=null;
+        for(int i=0; i<bookList.size();i++){
+            if(bookList.get(i)!=null&&bookList.get(i).getName().equals(bookName)) {
+                ret=bookList.get(i);
+                for(int j=i;j<bookList.size()-1; j++){
+                    bookList.set(j, bookList.get(j+1));
+                }
+                bookList.remove(bookList.size()-1);
+                break;
+            }
+        }
+        if (ret==null) {
+            System.out.println(getFIO()+". не брал книгу: "+bookName);
+        }
+        else System.out.println(getFIO()+"вернул книгу "+bookName);
+        return ret;
+    }
+    public void printStatus(){
+        String booksString="";
+        int count=0;
+        for (Book b:bookList){
+            if(b!=null){
+                booksString+=b.getName()+", ";
+                count++;
+            }
+        }
+        if (count==0){
+            System.out.println(getFIO()+" не брал книг");
+        }
+        else {
+            System.out.println(getFIO()+" взял "+count+" книг: "+booksString);
+        }
+    }
+    public String getName(){return name;}
+    public void setName (String name) {this.name=name;}
+
+    public String getSurname (){return surname;}
+    public void setSurname (String surname) {this.surname=surname;}
+
+    public String getDadName (){return dadName;}
+    public void setDadName (String dadName) {this.dadName=dadName;}
+
+    public String getFaculty (){return faculty;}
+    public void setFaculty (String faculty) {this.faculty=faculty;}
+
+    public String getBirthDate (){return birthDate;}
+    public void setBirthDate (String birthDate) {this.birthDate=birthDate;}
+
+    public String getPhone(){return phone;}
+    public void setPhone (String phone) {this.phone=phone;}
+
+    public int getTickNumber(){return tickNumber;}
+    public void setTickNumber (int tickNumber) {this.tickNumber=tickNumber;}
+}
---
+++ ex9_2/src/ex9_2.java
@@ -0,0 +1,150 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ex9_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Reader>readers= new ArrayList<Reader>();
+        int choose;
+        do {
+            System.out.println("Выберите действие:");
+            System.out.println("1-Добавить нового читателя");
+            System.out.println("2-Читатель хочет взять книгу");
+            System.out.println("3-Читатель хочет вернуть книгу");
+            System.out.println("4-Вывести статус читателя");
+            System.out.println("5-Вывести статус всех читателей");
+            System.out.println("6-Выйти из программы");
+            choose=sc.nextInt();
+            sc.nextLine();
+            switch (choose) {
+                case 1: {
+                    if (readers.size()<10) {
+                        readers.add(newReader(readers, sc));
+                        break;
+                    }
+                    else if (readers.size()==10) {
+                        System.out.println("Превышено количество читателей");
+                    }
+                    break;
+                }
+                case 2: {
+                    System.out.println("Введите номер читательского билета");
+                    int tickNumber = sc.nextInt();
+                    sc.nextLine();
+                    boolean found = false;
+                    for (Reader r: readers) {
+                        if (r != null && r.getTickNumber() == tickNumber) {
+                            found = true;
+                            r.takeBook(newBook(sc));
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Такого читателя нет");
+                    }
+                    break;
+                }
+                case 3: {
+                    System.out.println("Укажите название книги, которую желаете вернуть");
+                    String bookName = sc.nextLine();
+                    System.out.println("Введите номер читательского билета");
+                    int tickNumber = sc.nextInt();
+                    sc.nextLine();
+                    boolean found = false;
+                    for (Reader r: readers) {
+                        if (r != null && r.getTickNumber() == tickNumber) {
+                            found = true;
+                            r.returnBook(bookName);
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Такого пользователя нет");
+                    }
+                    break;
+                }
+                case 4: {
+                    System.out.println("Введите номер читательского билета");
+                    int tickNumber = sc.nextInt();
+                    sc.nextLine();
+                    boolean found = false;
+                    for (Reader r:readers) {
+                        if (r != null && r.getTickNumber() == tickNumber) {
+                            found = true;
+                            r.printStatus();
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Такого читателя нет");
+                    }
+                    break;
+                }
+                case 5: {
+                    for (Reader r: readers) {
+                        if (r != null){
+                            r.printStatus();
+                        }
+                    }
+                }
+                case 6: {
+                    System.out.println("Завершить программу");
+                    break;
+
+                }
+                default: {
+                    System.out.println("Нет такой команды");
+                }
+
+            }
+        }
+        while (choose !=6);
+    }
+    private static Reader newReader(ArrayList<Reader> readers, Scanner sc){
+        Reader reader=new Reader();
+        System.out.println("Введите имя");
+        reader.setName(sc.nextLine());
+        System.out.println("ВВедите фамилию");
+        reader.setSurname(sc.nextLine());
+        System.out.println("Введите отчество");
+        reader.setDadName(sc.nextLine());
+        System.out.println("Введите номер билета");
+        boolean isNumberFree;
+        int tickNumber;
+        do {
+            tickNumber=sc.nextInt();
+            sc.nextLine();
+            isNumberFree=true;
+            for(Reader r: readers){
+                if(r!=null && r.getTickNumber()==tickNumber) {
+                    System.out.println("Введите номер билета повторно");
+                    isNumberFree=false;
+                }
+            }
+        }
+        while (!isNumberFree);
+        reader.setTickNumber(tickNumber);
+
+        System.out.println("Введите название факультета");
+        reader.setFaculty(sc.nextLine());
+
+        System.out.println("Введите дату рождения");
+        reader.setBirthDate(sc.nextLine());
+
+        System.out.println("Введите номер телефона");
+        reader.setPhone(sc.nextLine());
+
+        return reader;
+    }
+    private static Book newBook (Scanner sc){
+        Book book=new Book();
+        System.out.println("Введите название книги");
+        book.setName(sc.nextLine());
+
+        System.out.println("Введите имя автора");
+        book.setAuthorName(sc.nextLine());
+
+        System.out.println("Введите содержание");
+        book.setText(sc.nextLine());
+        return book;
+    }
+}
