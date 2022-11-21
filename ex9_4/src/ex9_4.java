4badca2ceb36bf8eaacacfff6cec0436bff6b368
Кирилл Шевель <shevelkirill2011@gmail.com>
2022-11-21 18:38:53 +0300

    9.4 rework

--- ex9_4/src/ex9_4.java
+++ ex9_4/src/ex9_4.java
@@ -3,7 +3,8 @@ public class ex9_4 {
         Integer[]arr={5,5,6,6,3,4,7,7,8};
         Transform<Integer> trans=new Transform(arr);
         System.out.println(trans.arrayToMap(arr));
-
+        String[]arr1={"KU", "KUKU", "KU", "KUKUKU", "KUKU", "KU"};
+        System.out.println(trans.arrayToMap(arr1));
     }
 
 }
